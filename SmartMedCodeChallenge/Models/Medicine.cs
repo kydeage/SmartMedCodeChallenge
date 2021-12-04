26d5054be89c6396fa7ebe559e8685dc0cba57e3
kydeage <reubenjkaye@gmail.com>
2021-12-04 17:17:35 +0100

    Add project files.

---
+++ SmartMedCodeChallenge/Models/Medicine.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartMedCodeChallenge.Models
+{
+    public class Medicine
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int Quantity { get; set; }
+    }
+}
